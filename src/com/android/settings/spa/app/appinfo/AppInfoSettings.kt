589e1923fbb622534c54ab2917184eb2107298b6
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-03-02 12:13:13 +0200

    spa: add per-app setting for exploit protection compatibility mode

--- src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
+++ src/com/android/settings/spa/app/appinfo/AppInfoSettings.kt
@@ -182,6 +182,7 @@ private fun AppInfoSettings(packageInfoPresenter: PackageInfoPresenter) {
         }
 
         Category(title = stringResource(R.string.exploit_protection_category_title)) {
+            AppExploitProtectionCompatModeSwitchPreference(app, packageInfoPresenter)
         }
 
         Category(title = stringResource(R.string.app_install_details_group_title)) {
