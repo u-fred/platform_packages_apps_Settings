078445acf48756b67228adfaf1caa50c2227e1db
Fred Underwood <underwoodfred@proton.me>
2024-06-03 09:41:34 +1000

    remove LPU.isAutoPinConfirmEnabled() overload calls

--- tests/robotests/src/com/android/settings/security/screenlock/AutoPinConfirmPreferenceControllerTest.java
+++ tests/robotests/src/com/android/settings/security/screenlock/AutoPinConfirmPreferenceControllerTest.java
@@ -93,12 +93,12 @@ public class AutoPinConfirmPreferenceControllerTest {
     @Test
     public void updateState_ChangingSettingState_shouldSetPreferenceToAppropriateCheckedState() {
         // When auto_pin_confirm setting is disabled, switchPreference is unchecked
-        when(mLockPatternUtils.isAutoPinConfirmEnabled(TEST_USER_ID)).thenReturn(false);
+        when(mLockPatternUtils.isAutoPinConfirmEnabled(TEST_USER_ID, true)).thenReturn(false);
         mController.updateState(mPreference);
         assertThat(mPreference.isChecked()).isFalse();
 
         // When auto_pin_confirm setting is enabled, switchPreference is checked
-        when(mLockPatternUtils.isAutoPinConfirmEnabled(TEST_USER_ID)).thenReturn(true);
+        when(mLockPatternUtils.isAutoPinConfirmEnabled(TEST_USER_ID, true)).thenReturn(true);
         mController.updateState(mPreference);
         assertThat(mPreference.isChecked()).isTrue();
     }
