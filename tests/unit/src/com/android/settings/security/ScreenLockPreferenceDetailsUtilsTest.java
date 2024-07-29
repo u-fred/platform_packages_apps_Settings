21eb6149b03968d6ca2f9dd3adca0e0317db9c6b
Fred Underwood <underwoodfred@proton.me>
2024-07-29 13:05:39 +1000

    improve ScreenLockPreferenceDetailsUtilsTest

--- tests/unit/src/com/android/settings/security/ScreenLockPreferenceDetailsUtilsTest.java
+++ tests/unit/src/com/android/settings/security/ScreenLockPreferenceDetailsUtilsTest.java
@@ -17,13 +17,17 @@
 package com.android.settings.security;
 
 import static com.android.internal.widget.LockDomain.Primary;
+import static com.android.internal.widget.LockDomain.Secondary;
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -31,16 +35,22 @@ import static org.mockito.Mockito.when;
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.UserInfo;
 import android.content.res.Resources;
+import android.os.Bundle;
 import android.os.UserManager;
 import android.os.storage.StorageManager;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.android.internal.widget.LockDomain;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.widget.LockscreenCredential;
 import com.android.settings.SettingsActivity;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.password.ChooseLockGeneric;
+import com.android.settings.password.ChooseLockSettingsHelper;
 import com.android.settings.security.screenlock.ScreenLockSettings;
 import com.android.settings.testutils.FakeFeatureFactory;
 import com.android.settings.testutils.ResourcesUtils;
@@ -74,6 +84,7 @@ public class ScreenLockPreferenceDetailsUtilsTest {
     private Context mContext;
 
     private ScreenLockPreferenceDetailsUtils mScreenLockPreferenceDetailsUtils;
+    private ScreenLockPreferenceDetailsUtils mScreenLockPreferenceDetailsUtilsSecondary;
 
     @Before
     public void setUp() {
@@ -91,24 +102,70 @@ public class ScreenLockPreferenceDetailsUtilsTest {
 
         final FakeFeatureFactory featureFactory = FakeFeatureFactory.setupForTest();
         when(featureFactory.securityFeatureProvider.getLockPatternUtils(mContext))
-                 .thenReturn(mLockPatternUtils);
+                .thenReturn(mLockPatternUtils);
 
         mScreenLockPreferenceDetailsUtils = new ScreenLockPreferenceDetailsUtils(mContext);
+        mScreenLockPreferenceDetailsUtilsSecondary = new ScreenLockPreferenceDetailsUtils(mContext,
+                Secondary);
     }
 
     @Test
     public void isAvailable_whenEnabled_shouldReturnTrue() {
         whenConfigShowUnlockSetOrChangeIsEnabled(true);
-        int userId = 1;
-        assertThat(mScreenLockPreferenceDetailsUtils.isAvailable(userId)).isTrue();
+
+        assertThat(mScreenLockPreferenceDetailsUtils.isAvailable(USER_ID)).isTrue();
     }
 
     @Test
     public void isAvailable_whenDisabled_shouldReturnFalse() {
         whenConfigShowUnlockSetOrChangeIsEnabled(false);
-        int userId = 1;
-        assertThat(mScreenLockPreferenceDetailsUtils.isAvailable(userId)).isFalse();
+        assertThat(mScreenLockPreferenceDetailsUtils.isAvailable(USER_ID)).isFalse();
     }
+
+    @Test
+    public void isAvailable_secondaryWhenDisabled_shouldReturnFalse() {
+        whenConfigShowUnlockSetOrChangeIsEnabled(false);
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.isAvailable(USER_ID)).isFalse();
+    }
+
+
+    @Test
+    public void isAvailable_secondaryForUserNotSupport_returnsFalse() {
+        whenConfigShowUnlockSetOrChangeIsEnabled(true);
+        when(mLockPatternUtils.checkUserSupportsBiometricSecondFactor(USER_ID, false))
+                .thenReturn(false);
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.isAvailable(USER_ID)).isFalse();
+    }
+
+    @Test
+    public void isAvailable_secondaryWithKeyguardDisabled_returnFalse() {
+        whenConfigShowUnlockSetOrChangeIsEnabled(true);
+        when(mLockPatternUtils.checkUserSupportsBiometricSecondFactor(USER_ID, false))
+                .thenReturn(true);
+        when(mLockPatternUtils.isBiometricKeyguardEnabled(USER_ID)).thenReturn(false);
+
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.isAvailable(USER_ID)).isFalse();
+    }
+
+    @Test
+    public void isAvailable_secondaryWithKeyguardEnabled_returnTrue() {
+        // Make RestrictedLockUtilsInternal#checkIfKeyguardFeaturesDisabled work.
+        when(mContext.getSystemService(Context.DEVICE_POLICY_SERVICE))
+                .thenReturn(null);
+        UserManager um = mock(UserManager.class);
+        when(mContext.getSystemService(Context.USER_SERVICE)).thenReturn(um);
+        UserInfo userInfo = mock(UserInfo.class);
+        when(userInfo.isManagedProfile()).thenReturn(false);
+        when(um.getUserInfo(anyInt())).thenReturn(userInfo);
+
+        whenConfigShowUnlockSetOrChangeIsEnabled(true);
+        when(mLockPatternUtils.checkUserSupportsBiometricSecondFactor(USER_ID, false))
+                .thenReturn(true);
+        when(mLockPatternUtils.isBiometricKeyguardEnabled(USER_ID)).thenReturn(true);
+
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.isAvailable(USER_ID)).isTrue();
+    }
+
 
     @Test
     public void getSummary_unsecureAndDisabledPattern_shouldReturnUnlockModeOff() {
@@ -129,6 +186,31 @@ public class ScreenLockPreferenceDetailsUtilsTest {
         when(mLockPatternUtils.isLockScreenDisabled(anyInt())).thenReturn(false);
 
         assertThat(mScreenLockPreferenceDetailsUtils.getSummary(USER_ID)).isEqualTo(summary);
+    }
+
+    @Test
+    public void getSummary_secondaryNotSecure_returnsOff() {
+        final String summary =
+                prepareString("unlock_set_unlock_mode_off", "unlockModeOff");
+
+        when(mLockPatternUtils.isSecure(USER_ID, Secondary)).thenReturn(false);
+        when(mLockPatternUtils.isLockScreenDisabled(anyInt(), eq(Secondary))).thenReturn(true);
+
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.getSummary(USER_ID)).isEqualTo(
+                summary);
+    }
+
+    @Test
+    public void getSummary_secondaryPin_returnsPin() {
+        final String summary =
+                prepareString("unlock_set_unlock_mode_pin", "unlockModePin");
+
+        when(mLockPatternUtils.isSecure(USER_ID, Secondary)).thenReturn(true);
+        when(mLockPatternUtils.getKeyguardStoredPasswordQuality(USER_ID, Secondary))
+                .thenReturn(DevicePolicyManager.PASSWORD_QUALITY_NUMERIC);
+
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.getSummary(USER_ID)).isEqualTo(
+                summary);
     }
 
     @Test
@@ -236,11 +318,22 @@ public class ScreenLockPreferenceDetailsUtilsTest {
     public void isPasswordQualityManaged_passwordQualityIsManaged_shouldReturnTrue() {
         final RestrictedLockUtils.EnforcedAdmin admin = new RestrictedLockUtils.EnforcedAdmin();
 
-        when(mDevicePolicyManager.getPasswordQuality(admin.component, USER_ID))
+        when(mDevicePolicyManager.getPasswordQuality(admin.component, USER_ID, Primary))
                 .thenReturn(DevicePolicyManager.PASSWORD_QUALITY_MANAGED);
 
         assertThat(mScreenLockPreferenceDetailsUtils.isPasswordQualityManaged(USER_ID, admin))
                 .isTrue();
+    }
+
+    @Test
+    public void isPasswordQualityManaged_secondaryPasswordQualityNotManaged_returnFalse() {
+        final RestrictedLockUtils.EnforcedAdmin admin = new RestrictedLockUtils.EnforcedAdmin();
+
+        when(mDevicePolicyManager.getPasswordQuality(admin.component, USER_ID, Secondary))
+                .thenReturn(DevicePolicyManager.PASSWORD_QUALITY_NUMERIC);
+
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary.isPasswordQualityManaged(USER_ID,
+                admin)).isFalse();
     }
 
     @Test
@@ -290,11 +383,38 @@ public class ScreenLockPreferenceDetailsUtilsTest {
     }
 
     @Test
+    public void openScreenLockSettings_secondary_sendsIntent() {
+        SettingsPreferenceFragment resultListener = mock(SettingsPreferenceFragment.class);
+        int requestCode = 1000;
+
+        mScreenLockPreferenceDetailsUtilsSecondary.openScreenLockSettings(SOURCE_METRICS_CATEGORY,
+                resultListener, requestCode);
+
+        assertFragmentLaunchRequestedForResult(ScreenLockSettings.class.getName(),
+                resultListener, requestCode);
+    }
+
+    @Test
     public void getLaunchScreenLockSettingsIntent_returnsIntent() {
         final Intent intent = mScreenLockPreferenceDetailsUtils.getLaunchScreenLockSettingsIntent(
                 SOURCE_METRICS_CATEGORY);
 
         assertFragmentLaunchIntent(intent, ScreenLockSettings.class.getName());
+    }
+
+    @Test
+    public void getLaunchScreenLockSettingsIntent_secondary_returnsIntent() {
+        final Intent intent =
+                mScreenLockPreferenceDetailsUtilsSecondary.getLaunchScreenLockSettingsIntent(
+                        SOURCE_METRICS_CATEGORY);
+
+        assertFragmentLaunchIntent(intent, ScreenLockSettings.class.getName());
+        assertThat(intent.getParcelableExtra(
+                ChooseLockSettingsHelper.EXTRA_KEY_LOCK_DOMAIN, LockDomain.class))
+                .isEqualTo(Secondary);
+        assertThat(intent.getBooleanExtra(
+                ChooseLockSettingsHelper.EXTRA_KEY_FOREGROUND_ONLY, false))
+                .isEqualTo(true);
     }
 
     @Test
@@ -307,6 +427,20 @@ public class ScreenLockPreferenceDetailsUtilsTest {
     }
 
     @Test
+    public void openChooseLockGenericFragment_secondary_sendsIntent() {
+        LockscreenCredential credential = LockscreenCredential.createPassword("password");
+        SettingsPreferenceFragment resultListener = mock(SettingsPreferenceFragment.class);
+        int requestCode = 1000;
+
+        assertThat(mScreenLockPreferenceDetailsUtilsSecondary
+                .openChooseLockGenericFragment(SOURCE_METRICS_CATEGORY, credential, resultListener,
+                        requestCode)).isTrue();
+        assertFragmentLaunchRequestedForResult(
+                ChooseLockGeneric.ChooseLockGenericFragment.class.getName(), resultListener,
+                requestCode);
+    }
+
+    @Test
     public void getLaunchChooseLockGenericFragmentIntent_noQuietMode_returnsIntent() {
         when(mUserManager.isQuietModeEnabled(any())).thenReturn(false);
 
@@ -315,6 +449,25 @@ public class ScreenLockPreferenceDetailsUtilsTest {
 
         assertFragmentLaunchIntent(intent,
                 ChooseLockGeneric.ChooseLockGenericFragment.class.getName());
+    }
+
+    @Test
+    public void getChooseLockGenericFragmentIntent_secondary_returnsIntent() {
+        LockscreenCredential credential = LockscreenCredential.createPassword("password");
+        final Intent intent =
+                mScreenLockPreferenceDetailsUtilsSecondary.getChooseLockGenericFragmentIntent(
+                        SOURCE_METRICS_CATEGORY, credential);
+
+        assertFragmentLaunchIntent(intent,
+                ChooseLockGeneric.ChooseLockGenericFragment.class.getName());
+        assertThat(intent.getParcelableExtra(
+                ChooseLockSettingsHelper.EXTRA_KEY_LOCK_DOMAIN, LockDomain.class))
+                .isEqualTo(Secondary);
+        Bundle args = intent.getBundleExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_ARGUMENTS);
+        assertNotNull(args);
+        LockscreenCredential credentialActual = args.getParcelable(
+                ChooseLockSettingsHelper.EXTRA_KEY_PASSWORD, LockscreenCredential.class);
+        assertEquals(credential, credentialActual);
     }
 
     private void whenConfigShowUnlockSetOrChangeIsEnabled(boolean enabled) {
@@ -334,6 +487,15 @@ public class ScreenLockPreferenceDetailsUtilsTest {
     private void assertFragmentLaunchRequested(String fragmentClassName) {
         ArgumentCaptor<Intent> intentCaptor = ArgumentCaptor.forClass(Intent.class);
         verify(mContext).startActivity(intentCaptor.capture());
+
+        Intent intent = intentCaptor.getValue();
+        assertFragmentLaunchIntent(intent, fragmentClassName);
+    }
+
+    private void assertFragmentLaunchRequestedForResult(String fragmentClassName,
+            SettingsPreferenceFragment resultListener, int requestCode) {
+        ArgumentCaptor<Intent> intentCaptor = ArgumentCaptor.forClass(Intent.class);
+        verify(resultListener).startActivityForResult(intentCaptor.capture(), eq(requestCode));
 
         Intent intent = intentCaptor.getValue();
         assertFragmentLaunchIntent(intent, fragmentClassName);
