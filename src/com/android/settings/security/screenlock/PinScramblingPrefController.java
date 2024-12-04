7bbb51b71fc4944e5639af301bdc9c4651502612
Fred Underwood <underwoodfred@proton.me>
2024-12-04 10:28:44 +1000

    add second factor to existing screenlock preference controllers

--- src/com/android/settings/security/screenlock/PinScramblingPrefController.java
+++ src/com/android/settings/security/screenlock/PinScramblingPrefController.java
@@ -1,22 +1,28 @@
 package com.android.settings.security.screenlock;
 
+import static com.android.internal.widget.LockDomain.Primary;
+
 import android.content.Context;
 import android.ext.settings.ExtSettings;
 
+import com.android.internal.widget.LockDomain;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.widget.WrappedLockPatternUtils;
 import com.android.settings.ext.BoolSettingPrefController;
 import com.android.settings.overlay.FeatureFactory;
 
 public class PinScramblingPrefController extends BoolSettingPrefController {
 
-    private final LockPatternUtils lockPatternUtils;
+    private final WrappedLockPatternUtils lockPatternUtils;
 
-    public PinScramblingPrefController(Context ctx, String key) {
-        super(ctx, key, ExtSettings.SCRAMBLE_LOCKSCREEN_PIN_LAYOUT);
+    static final String PREF_KEY = "scramble_pin_layout";
 
-        lockPatternUtils = FeatureFactory.getFeatureFactory()
-                .getSecurityFeatureProvider()
-                .getLockPatternUtils(ctx);
+    public PinScramblingPrefController(Context ctx, WrappedLockPatternUtils wlpu) {
+        super(ctx, PREF_KEY, wlpu.getLockDomain() == Primary ?
+                ExtSettings.SCRAMBLE_LOCKSCREEN_PIN_LAYOUT_PRIMARY :
+                ExtSettings.SCRAMBLE_LOCKSCREEN_PIN_LAYOUT_SECONDARY);
+
+        lockPatternUtils = wlpu;
     }
 
     @Override
